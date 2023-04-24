d02e34bcd248152f3e1928fb891b4d80ae1b93bb
NiNejah <almawlanejeh@gmail.com>
2023-04-24 18:42:35 +0200

    update : function.sql

--- sql/function.sql
+++ sql/function.sql
@@ -1,52 +1,68 @@
+
 -- For Stored procedure EXO 3
 -- Create PopulationDepartement column in Departement 
--- And PopulationRegion column in Region
 -- It will be called by the user (main.py) 
  
-CREATE OR REPLACE FUNCTION CalculatePopulation(years integer[])
-RETURNS void AS $$
+CREATE OR REPLACE PROCEDURE CalculateDepartementPopulation(year integer)
+AS $$
 DECLARE
     dep RECORD; -- record to hold a row of the Departement table during iteration
+BEGIN
+    -- Create the necessary column in the table if they don't already exist
+    BEGIN
+        ALTER TABLE Departement ADD COLUMN PopulationDepartement FLOAT;
+    EXCEPTION WHEN duplicate_column THEN
+        -- Do nothing if the column already exists
+        NULL;
+    END;
+    -- Calculate the population for each department
+    FOR dep IN SELECT * FROM Departement LOOP
+        UPDATE Departement
+        SET PopulationDepartement = (
+            SELECT SUM(Statistic.StatValue)
+            FROM Commune
+            INNER JOIN Statistic
+                ON Commune.CodeCommune = Statistic.CodeCommune
+            WHERE Commune.IdDepartement = dep.IdDepartement
+            AND Indicator = 'Population'
+            AND StartYear = year
+            AND (EndYear IS NULL OR EndYear >= year)
+        )
+        WHERE IdDepartement = dep.IdDepartement;
+    END LOOP;
+    
+END;
+$$ LANGUAGE plpgsql;
+
+-- Create PopulationRegion column in Region
+
+CREATE OR REPLACE PROCEDURE CalculateRegionPopulation(year integer)
+AS $$
+DECLARE
     reg RECORD; -- record to hold a row of the Region table during iteration
 BEGIN
-    -- Create the necessary columns in the tables if they don't already exist
+    -- Create the necessary column in the table if they don't already exist
     BEGIN
-        ALTER TABLE Departement ADD COLUMN IF NOT EXISTS PopulationDepartement FLOAT;
+        ALTER TABLE Region ADD COLUMN PopulationRegion FLOAT;
+    EXCEPTION WHEN duplicate_column THEN
+        -- Do nothing if the column already exists
+        NULL;
     END;
-    
-    BEGIN
-        ALTER TABLE Region ADD COLUMN IF NOT EXISTS PopulationRegion FLOAT;
-    END;
-
-    FOREACH year IN ARRAY years LOOP
-        -- Calculate the population for each department for the current year
-        FOR dep IN SELECT * FROM Departement LOOP
-            UPDATE Departement
-            SET PopulationDepartement = (
-                SELECT SUM(Statistic.StatValue)
-                FROM Commune
-                INNER JOIN Statistic
-                    ON Commune.CodeCommune = Statistic.CodeCommune
-                WHERE Commune.IdDepartement = dep.IdDepartement
-                AND Indicator = 'Population'
-                AND StartYear = year
-                AND (EndYear IS NULL OR EndYear >= year)
-            )
-            WHERE IdDepartement = dep.IdDepartement;
-        END LOOP;
-
-        -- Calculate the population for each region for the current year
-        FOR reg IN SELECT * FROM Region LOOP
-            UPDATE Region
-            SET PopulationRegion = (
-                SELECT SUM(PopulationDepartement)
-                FROM Departement
-                WHERE CodeRegion = reg.CodeRegion
-            )
-            WHERE CodeRegion = reg.CodeRegion;
-        END LOOP;
+    CALL CalculateDepartementPopulation(year); 
+    -- Calculate the population for each region
+    FOR reg IN SELECT * FROM Region LOOP
+        UPDATE Region
+        SET PopulationRegion = (
+            SELECT SUM(PopulationDepartement)
+            FROM Departement
+            WHERE IdRegion = reg.IdRegion
+        )
+        WHERE IdRegion = reg.IdRegion;
     END LOOP;
 END;
 $$ LANGUAGE plpgsql;
 
-CALL CalculatePopulation(array[2008, 2013, 2019]);
+
+CALL CalculateRegionPopulation(2019);
+select * from region;
+SELECT * FROM Departement ; 
